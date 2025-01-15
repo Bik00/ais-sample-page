584c0e63392ae93f655cbb1987a112b0da077d93
Bikoo <bikoo3002@naver.com>
2025-01-15 13:12:11 +0900

    적용 제품 정보 수정

--- js/result.js
+++ js/result.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-
+    $(document).on("click", "input[type='button']", function () {
+        const value = $(this).val();
+        $("#productModalLabel").text("선택된 항목");
+        $(".modal-body").text(`현재 선택된 항목은 "${value}"입니다.`);
+    });
 });
